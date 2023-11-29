ede5019900b1856be7434a2a287b3be3aa2c262d
Liya <Liyatsegaye2301@gmail.com>
2023-11-29 16:38:29 +0300

    bmi

---
+++ JS1.js
@@ -0,0 +1,17 @@
+
+var num =1
+if (num==1){
+    var n1 = 3
+    let n2= 2
+    const n3=4
+    var n1 =4
+
+}
+let n5=5
+const t =3
+function ages(){
+var age = 25
+let age1 = 60
+const age2 =45
+}
+console.log(n5)
---
+++ Js2.js
@@ -0,0 +1,6 @@
+
+
+ const mass = prompt("What is your mass?");
+ const height = prompt("What is your height?");
+ const BMI = mass / (height*height);
+ console.log(BMI);
